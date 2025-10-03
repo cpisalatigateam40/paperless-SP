3b170c5c2d4066a22c57270c63bfd103b1229be9
unknown <rizalfahadian7@gmail.com>
2025-10-03 14:21:30 +0700

    sync plant

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\Api\ApiController;
 
 Route::post('/user-sync', [ApiController::class, 'syncUser']);
 Route::post('/user-desync', [ApiController::class, 'desyncUser']);
+
+Route::post('/plant-sync', [ApiController::class, 'syncPlant']);
