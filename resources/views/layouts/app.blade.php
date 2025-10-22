6d1dda1b29eecf5ffb51755eec7cda91440a6b84
unknown <rizalfahadian7@gmail.com>
2025-10-22 10:55:10 +0700

    favison

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link rel="icon" type="image/png" href="{{ asset('img/sausage.png') }}">
+
 
     <style>
     body,
