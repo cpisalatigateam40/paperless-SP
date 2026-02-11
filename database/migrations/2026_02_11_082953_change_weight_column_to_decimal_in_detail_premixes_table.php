17d2e3418eedfc27d5bee75e0927e016ae8c26b5
unknown <rizalfahadian7@gmail.com>
2026-02-11 08:37:57 +0700

    fix data type

---
+++ database/migrations/2026_02_11_082953_change_weight_column_to_decimal_in_detail_premixes_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('detail_premixes', function (Blueprint $table) {
+            $table->decimal('weight', 8, places: 2)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('detail_premixes', function (Blueprint $table) {
+            $table->float('weight')->nullable()->change();
+        });
+    }
+};
