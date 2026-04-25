77447779ac837347f38253dd3e923ebd0579e85e
unknown <rizalfahadian7@gmail.com>
2026-04-25 08:11:43 +0700

    add nullable to sample_type

---
+++ database/migrations/2026_04_25_080920_change_nullable_on_detail_lab_samples_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('detail_lab_samples', function (Blueprint $table) {
+            $table->string('sample_type')->nullable()->change();
+            $table->string('unit')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('detail_lab_samples', function (Blueprint $table) {
+            $table->string('sample_type')->nullable(false)->change();
+            $table->string('unit')->nullable(false)->change();
+        });
+    }
+};
